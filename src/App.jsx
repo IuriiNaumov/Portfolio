9027319771d7506316922967e5c48e75eb2f6e8e
Naumov Yura <naumoviiury@gmail.com>
2023-10-08 19:25:13 +0600

    парвит стили и добавляет аватар

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "./Header";
 import SelfInfo from "./SelfInfo";
 import styles from "./App.module.css";
+import About from "./About";
 
 function App() {
     return (
         <div className={styles.App}>
             <Header />
             <SelfInfo />
+            <About />
         </div>
     );
 }
