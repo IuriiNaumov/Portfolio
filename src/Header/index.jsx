551af3e2022e1c3c330cd5917abc63f260161e28
Yura <naumoviiury@gmail.com>
2023-10-08 19:28:37 +0600

    Add files via upload

--- src/Header/index.jsx
+++ src/Header/index.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import styles from "./component.module.css";
+import { PeopleCircle1Icon32Regular } from "@skbkontur/icons";
 
 function Header() {
     return (
         <div className={styles.header}>
-            <strong>Iurii.dev</strong>
+            <div className={styles.logo}>
+                <strong className={styles.title}>Iurii.dev</strong>
+                <PeopleCircle1Icon32Regular />
+            </div>
             <div className={styles.menu}>
                 <span>Home</span>
                 <span>About</span>
--- src/SelfInfo/index.jsx
+++ src/SelfInfo/index.jsx
@@ -2,20 +2,26 @@ import React from "react";
 import styles from "./component.module.css";
 import Links from "../Links";
 import TechStack from "../TechStack";
+import selfIcon from "../img/self.jpg";
 
 function SelfInfo() {
     return (
         <div className={styles.wrapper}>
-            <span className={styles.title}>
-                Front-End React Developer <span>&#128640;</span>
-            </span>
-            <span className={styles.subtitle}>Hey I'm Iuri Naumov. A passionate Front-end React Developer based in Astana/Kazakhstan &#128205;</span>
-            <React.Fragment>
-                <Links />
-            </React.Fragment>
-            <React.Fragment>
-                <TechStack />
-            </React.Fragment>
+            <div className={styles.infoBlock}>
+                <span className={styles.title}>
+                    Front-End React Developer <span>&#128640;</span>
+                </span>
+                <span className={styles.subtitle}>Hey I'm Iuri Naumov. A passionate Front-end React Developer based in Astana/Kazakhstan &#128205;</span>
+                <React.Fragment>
+                    <Links />
+                </React.Fragment>
+                <React.Fragment>
+                    <TechStack />
+                </React.Fragment>
+            </div>
+            <div>
+                <img src={selfIcon} className={styles.avatar}></img>
+            </div>
         </div>
     );
 }
